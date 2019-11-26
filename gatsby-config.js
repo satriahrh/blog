b30dcdc396347fb34444f50d6a1d2ee60649cdda
Satria H R Harsono <satriahrh@gmail.com>
2019-11-26 22:33:06 +0700

    add satriahrh icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,13 +57,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/satriahrh-icon.png`,
       },
     },
     `gatsby-plugin-offline`,
